2e5525eab448b8baa8c2aea6f46ee69ff30c0907
Jerome Brown <Jerome@SoliDeoGloria.tech>
2025-02-17 15:23:02 +1300

    Download Editor Services as required

--- src/powershell.rs
+++ src/powershell.rs
@@ -1,8 +1,6 @@
-mod language_server;
-
-use zed_extension_api::{self as zed, settings::LspSettings};
-
-use crate::language_server::PowerShellEditorServices as EditorServices;
+use std::fs;
+use std::path;
+use zed_extension_api::{self as zed, Result};
 
 struct PowerShellExtension {
     /// The PowerShell binary, default to `pwsh`.
@@ -16,7 +14,7 @@ impl zed::Extension for PowerShellExtension {
         Self: Sized,
     {
         Self {
-            powershell_bin: Some("pwsh".to_string()),
+            powershell_bin: None,
         }
     }
 
@@ -25,21 +23,13 @@ impl zed::Extension for PowerShellExtension {
         language_server_id: &zed_extension_api::LanguageServerId,
         worktree: &zed_extension_api::Worktree,
     ) -> zed_extension_api::Result<zed_extension_api::Command> {
-        let pwsh_bin = worktree
-            .which(self.powershell_bin.clone().unwrap().as_str())
-            .ok_or_else(|| "No PowerShell command found")?;
+        let pwsh_bin = PowerShellExtension::powershell_binary_path(self, worktree).unwrap();
 
-        let bundle_path = LspSettings::for_worktree("powershell-es", worktree)
-            .ok()
-            .and_then(|lsp_settings| lsp_settings.binary)
-            .and_then(|binary| binary.path)
-            .unwrap();
+        let bundle_path = self
+            .language_server_path(language_server_id)
+            .map_err(|err| format!("failed to get editor services: {}", err))?;
 
-        // TODO: make remote install works.
-        // let bundle_path = EditorServices::install(language_server_id)
-        //     .map_err(|err| format!("failed to get editor services: {}", err))?;
-
-        let command = format!("{bundle_path}/PowerShellEditorServices/Start-EditorServices.ps1 -BundledModulesPath {bundle_path} -Stdio -SessionDetailsPath {bundle_path}/powershell-es.session.json  -LogPath {bundle_path}/logs -FeatureFlags @() -AdditionalModules @() -HostName zed -HostProfileId 0 -HostVersion 1.0.0 -LogLevel Diagnostic");
+        let command = format!("Import-Module (Join-Path '{bundle_path}' 'PowerShellEditorServices/PowerShellEditorServices.psd1'); Start-EditorServices -Stdio -SessionDetailsPath '{bundle_path}/powershell-es.session.json' -LogPath '{bundle_path}/logs' -FeatureFlags @() -AdditionalModules @() -HostName zed -HostProfileId 0 -HostVersion 1.0.0 -LogLevel Diagnostic");
 
         Ok(zed::Command {
             command: pwsh_bin,
@@ -51,6 +41,77 @@ impl zed::Extension for PowerShellExtension {
             ],
             env: Default::default(),
         })
+    }
+}
+
+impl PowerShellExtension {
+    fn powershell_binary_path(&mut self, worktree: &zed::Worktree) -> Result<String> {
+        let pwsh_path = match &self.powershell_bin {
+            Some(path) if fs::metadata(path).map_or(false, |stat| stat.is_file()) => path.clone(),
+            Some(path) => worktree
+                .which(path.clone().as_str())
+                .ok_or_else(|| "PowerShell must be installed for PowerShell Extension")?,
+            None => worktree
+                .which("pwsh")
+                .ok_or_else(|| "PowerShell must be installed for PowerShell Extension")?,
+        };
+        self.powershell_bin = Some(pwsh_path.clone());
+        Ok(pwsh_path)
+    }
+
+    fn language_server_path(
+        &mut self,
+        language_server_id: &zed_extension_api::LanguageServerId,
+    ) -> Result<String> {
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+
+        let release = zed::latest_github_release(
+            "PowerShell/PowerShellEditorServices",
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == "PowerShellEditorServices.zip")
+            .ok_or_else(|| format!("no PowerShellEditorServices.zip found"))?;
+
+        let version_dir = format!("powershell-es-{}", release.version);
+        let lsp_path = format!("{version_dir}/PowerShellEditorServices/Start-EditorServices.ps1");
+
+        if !fs::metadata(&lsp_path).map_or(false, |stat| stat.is_file()) {
+            // Download the asset
+            zed::set_language_server_installation_status(
+                &language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+            zed::download_file(
+                &asset.download_url,
+                &version_dir,
+                zed::DownloadedFileType::Zip,
+            )
+            .map_err(|err| format!("download error {}", err))?;
+
+            // Ensure the binary exists
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(entry.path()).ok();
+                }
+            }
+        }
+
+        let abs_path =
+            path::absolute(&version_dir).map_err(|e| format!("failed to get absolute path {e}"))?;
+        Ok(abs_path.display().to_string())
     }
 }
 
